d09885982c576478ecc6ada52e480093fe0733d8
amazing <305055926@qq.com>
2020-01-01 21:49:00 +0800

    first commit

---
+++ src/main/java/com/ruoyi/TaoshaMallApplication.java
@@ -0,0 +1,27 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class TaoshaMallApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(TaoshaMallApplication.class, args);
+        System.out.println("\n" +
+                " █████╗ ███╗   ███╗ █████╗ ███████╗██╗███╗   ██╗ ██████╗ \n" +
+                "██╔══██╗████╗ ████║██╔══██╗╚══███╔╝██║████╗  ██║██╔════╝ \n" +
+                "███████║██╔████╔██║███████║  ███╔╝ ██║██╔██╗ ██║██║  ███╗\n" +
+                "██╔══██║██║╚██╔╝██║██╔══██║ ███╔╝  ██║██║╚██╗██║██║   ██║\n" +
+                "██║  ██║██║ ╚═╝ ██║██║  ██║███████╗██║██║ ╚████║╚██████╔╝\n" +
+                "╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ \n");
+    }
+}
